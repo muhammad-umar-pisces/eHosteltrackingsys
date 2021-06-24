aaf88ed84c840e51aa8454583b8d5ad8baab43f1
M.Umar <umar032190@gmail.com>
2021-06-24 05:06:01 -0400

    chatbot integration test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
 	before_action :authenticate_user!
 	 protect_from_forgery with: :exception
+
+   def chatbot
+     debugger
+   end
+
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password) }
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation) }
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
         registrations: 'users/registrations'
       }
 
-
+  post 'chatbot'
   get 'index/index'
 	root 'index#index'
   get 'girls/index'
